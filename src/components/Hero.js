907be3848b8ab5705d9dd00af6ffebeb45942244
Aaronarphaxad <aaronarphaxad@gmail.com>
2024-12-27 22:09:02 -0800

    fix padding

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -26,7 +26,7 @@ export default function Hero() {
   }, []);
 
   return (
-    <div className="relative min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 pt-60 md:pt-0">
+    <div className="relative min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 pt-80 md:pt-0">
       <div className="absolute inset-0 bg-grid-pattern opacity-[0.02]" />
       
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
